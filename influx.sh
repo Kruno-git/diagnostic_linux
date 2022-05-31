f022a1d94598d01ffa3df0e2089c5e6db826164a
Kruno-git <81048049+Kruno-git@users.noreply.github.com>
2022-05-31 08:52:39 +0200

    Update influx.sh

--- influx.sh
+++ influx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
-makepkg -si
+makepkg -si # You may get an error running it under sudo/root
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install influxdb --edge
 sudo systemctl enable influxdb && systemctl start influxdb
-sudo pacman -Sy grafana 
+sudo pacman -Sy grafana
+systemctl start grafana && systemctl enable grafana
